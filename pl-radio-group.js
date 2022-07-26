681f9e1b4ab22cf6a7c381bccc1672acd65d2235
Azat Yamalov <yamalov.azat@live.com>
2022-07-26 13:58:05 +0300

    readonly

--- pl-radio-group.js
+++ pl-radio-group.js
@@ -3,105 +3,113 @@ import { PlElement, html, css } from "polylib";
 import "@plcmp/pl-labeled-container";
 
 class PlRadioGroup extends PlElement {
-	static get properties() {
-		return {
-			label: { type: String },
-			variant: { type: String },
-			vertical: { type: Boolean, reflectToAttribute: true },
-			required: { type: Boolean, value: false, observer: '_requiredObserver' },
-			disabled: { type: Boolean, reflectToAttribute: true },
-			selected: { type: String, observer: '_selectedObserver' },
-			invalid: { type: Boolean, value: false }
+	static properties = {
+		label: { type: String },
+		variant: { type: String },
+		orientation: { type: String },
+		vertical: { type: Boolean, reflectToAttribute: true },
+		required: { type: Boolean, value: false, observer: '_requiredObserver' },
+		disabled: { type: Boolean, reflectToAttribute: true },
+		selected: { type: String, observer: '_selectedObserver' },
+		invalid: { type: Boolean, value: false },
+		readonly: { type: Boolean, reflectToAttribute: true }
+	}
+
+	static css = css`
+		:host([disabled]) {
+			color: var(--grey-base);
+			cursor: not-allowed;
+			pointer-events: none;
+			user-select: none;
 		}
-	}
 
-	static get css() {
-		return css`
-			:host([disabled]) {
-				color: var(--grey-base);
-				cursor: not-allowed;
-				pointer-events: none;
-				user-select: none;
-			}
+		:host([vertical]) .radio-container {
+			flex-direction: column;
+		}
 
+		.radio-container ::slotted(pl-radio-button:first-child) {
+			border-radius: 4px 0 0 4px;
+			border-left: 1px solid var(--grey-base);
+		}
 
-			:host([vertical]) .radio-container {
-				flex-direction: column;
-			}
+		.radio-container ::slotted(pl-radio-button:last-child) {
+			border-radius: 0 4px 4px 0;
+			border-right: 1px solid var(--grey-base);
+		}
 
-			.radio-container ::slotted(pl-radio-button:first-child) {
-				border-radius: 4px 0 0 4px;
-				border-left: 1px solid var(--grey-base);
-			}
+		:host([readonly]) .radio-container {
+			pointer-events: none;
+		}
 
-			.radio-container ::slotted(pl-radio-button:last-child) {
-				border-radius: 0 4px 4px 0;
-				border-right: 1px solid var(--grey-base);
-			}
+		.radio-container {
+			display: flex;
+			flex-direction: row;
+			background: var(--background-color);
+			border: 1px solid transparent;
+			position: relative;	
+			overflow: hidden;
+		}
 
-			.radio-container {
-				display: flex;
-				flex-direction: row;
-				background: var(--background-color);
-				border: 1px solid transparent;
-				position: relative;	
-				overflow: hidden;
-			}
+		.radio-container.required {
+			border: 1px solid var(--grey-base);
+			border-radius: var(--border-radius);
+		}
 
-			.radio-container.required {
-                border: 1px solid var(--grey-base);
-				border-radius: var(--border-radius);
-			}
+		.radio-container.required ::slotted(pl-radio-button) {
+			border-top: 1px solid transparent;
+			border-bottom: 1px solid transparent;
+		}
 
-			.radio-container.required ::slotted(pl-radio-button) {
-                border-top: 1px solid transparent;
-				border-bottom: 1px solid transparent;
-			}
+		.radio-container.required ::slotted(pl-radio-button:first-child) {
+			border-left: 1px solid transparent;
+		}
 
-			.radio-container.required ::slotted(pl-radio-button:first-child) {
-                border-left: 1px solid transparent;
-			}
+		.radio-container.required ::slotted(pl-radio-button:last-child) {
+			border-right: 1px solid transparent;
+		}
 
-			.radio-container.required ::slotted(pl-radio-button:last-child) {
-                border-right: 1px solid transparent;
-			}
+		.radio-container::before {
+			content: '';
+			display: block;
+			position: absolute;
+			box-sizing: border-box;
+			inset-block-start: 0;
+			inset-inline-start: 0;
+		}
 
-			.radio-container::before {
-				content: '';
-				display: block;
-				position: absolute;
-				box-sizing: border-box;
-                inset-block-start: 0;
-                inset-inline-start: 0;
-			}
+		.radio-container.required::before {
+			border-block-start: calc(var(--space-md) / 2) solid var(--attention);
+			border-inline-start: calc(var(--space-md) / 2)  solid var(--attention);
+			border-inline-end: calc(var(--space-md) / 2) solid transparent;
+			border-block-end: calc(var(--space-md) / 2) solid transparent;
+		}
+	`;
 
-			.radio-container.required::before {
-				border-block-start: calc(var(--space-md) / 2) solid var(--attention);
-				border-inline-start: calc(var(--space-md) / 2)  solid var(--attention);
-				border-inline-end: calc(var(--space-md) / 2) solid transparent;
-				border-block-end: calc(var(--space-md) / 2) solid transparent;
-			}
-		`;
-	}
-
-	static get template() {
-		return html`
-			<pl-labeled-container label="[[label]]" variant$="[[variant]]">
-				<slot name="label-prefix" slot="label-prefix"></slot>
-				<div class="radio-container">
-					<slot></slot>
-				</div>
-				<slot name="label-suffix" slot="label-suffix"></slot>
-			</pl-labeled-container>
-		`;
-	}
+	static template = html`
+		<pl-labeled-container label="[[label]]" orientation="[[variant]]">
+			<slot name="label-prefix" slot="label-prefix"></slot>
+			<div class="radio-container" id="container">
+				<slot></slot>
+			</div>
+			<slot name="label-suffix" slot="label-suffix"></slot>
+		</pl-labeled-container>
+	`;
 
 	connectedCallback() {
 		super.connectedCallback();
 		this._radioButtons = this.root.querySelector('.radio-container slot').assignedElements();
-		this._radioContainer = this.root.querySelector('.radio-container');
+		this._radioContainer = this.$.container;
+
+		if (this.variant) {
+			console.log('Variant is deprecated, use orientation instead');
+			this.orientation = this.variant;
+		}
+
 
 		this.addEventListener('radio-selected', (ev) => {
+			if (this.readonly) {
+				return;
+			}
 			this.selected = ev.detail.name;
 		});
 		this.validate();
